977c32cdf98b9237a254a864a46e830031bcb80e
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-02-22 13:03:49 +0100

    commit

--- ui.py
+++ ui.py
@@ -29,6 +29,61 @@ class CreateClientWindow(Toplevel, CenterWidgetMixin):
             frame=Frame(self)
             frame.pack(padx=20, pady=10)  #para darle margen a la ventana
 
+            Label(frame, text="DNI(2 números y una letra mayusc)").grid(row=0, column=0) #para poner un label
+            Label(frame, text="Nombre").grid(row=0, column=1)
+            Label(frame, text="Apellidos").grid(row=0, column=2)
+
+            dni=Entry(frame)
+            dni.grid(row=1, column=0)
+            dni.bind("<KeyRelease>", lambda event: self.validate(event,0))
+            nombre=Entry(frame)
+            nombre.grid(row=1, column=1)
+            nombre.bind("<KeyRelease>", lambda event: self.validate(event,1))
+            apellido=Entry(frame)
+            apellido.grid(row=1, column=2)
+            apellido.bind("<KeyRelease>", lambda event: self.validate(event,2))
+
+            frame=Frame(self)
+            frame.pack(pady=10)
+
+            crear=Button(frame, text="Crear", command=self.create_client)
+            crear.configure(state=DISABLED)
+            crear.grid(row=0, column=0)
+            Button(frame, text="Cancelar", command=self.close).grid(row=0, column=1) #para poner un boton
+
+            self.validaciones=[0, 0, 0]
+            self.crear=crear
+            self.dni=dni
+            self.nombre=nombre
+            self.apellido=apellido
+
+        def create_client(self):
+            self.master.treeview.insert(
+                parent='', index='end', iid=self.dni.get(),
+                values=(self.dni.get(), self.nombre.get(), self.apellido.get()))
+            db.Clientes.crear(self.dni.get(), self.nombre.get(), self.apellido.get())
+            self.close()
+
+        def close(self):
+            self.destroy()
+            self.update()
+        
+        def validate (self, event, index):
+            valor=event.widget.get()
+            valido=helpers.dni_valido(valor, db.Clientes.lista) if index == 0 else (valor.isalpha() and len(valor) >= 2 and len(valor) <= 30)
+            event.widget.configure({"bg": "Green" if valido else "Red"}) #para cambiar el color del fondo
+            self.validaciones[index]=valido
+            self.crear.config(state=NORMAL if self.validaciones == [1,1,1] else DISABLED)
+
+    class EditClientWindow(Toplevel, CenterWidgetMixin):
+        def __init__ (self, parent):
+            super().__init__(parent)
+            self.title('Actualizar cliente')
+            self.build()
+            self.center()
+            self.transient(parent)
+            self.grab_set()
+
             
 
 if __name__ == '__main__':
