2df1fe75f0e7b3477c3fc3d036504767401c16bf
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-02-25 12:23:29 +0100

    commit

--- tests/test_database.py
+++ tests/test_database.py
@@ -8,53 +8,54 @@ import helpers
 class TestDatabase(unittest.TestCase):
     def setUp(self):  #setUp es un método que se ejecuta antes de cada test
         db.Clientes.lista=[     #se crea una lista de clientes para poder hacer las pruebas
-            db.Cliente("15J", "Juan", "Perez"),
-            db.Cliente("48H", "Maria", "Gomez"),
-            db.Cliente("28Z", "Pedro", "Gonzalez"),
+            db.Cliente("15J", "Marta", "Perez"),
+            db.Cliente("48H", "Manolo", "Lopez"),
+            db.Cliente("28Z", "Ana", "Garcia"), #se crea un cliente no existente
         ]
     def test_buscar_cliente(self):   #se crea un test para comprobar que se puede buscar un cliente
         cliente_existente=db.Clientes.buscar("15J")    #se busca un cliente que existe
-        cliente_no_existente=db.Clientes.buscar("15K")   #se busca un cliente que no existe
+        cliente_inexistente=db.Clientes.buscar("99X")   #se busca un cliente que no existe
         self.assertIsNotNone(cliente_existente) #assertIsNotNone comprueba que el objeto no es None, se usa para comprobar que existe
-        self.assertIsNone(cliente_no_existente) #assertIsNone comprueba que el objeto es None, se usa para comprobar que no existe
+        self.assertIsNone(cliente_inexistente) #assertIsNone comprueba que el objeto es None, se usa para comprobar que no existe
     def test_crear_cliente(self):
-        nuevo_cliente=db.Clientes.crear('18H', 'Héctor', 'Lopez') #se crea un nuevo cliente
+        nuevo_cliente=db.Clientes.crear('39X', 'Héctor', 'Costa') #se crea un nuevo cliente
         self.assertEqual(len(db.Clientes.lista), 4) #se comprueba que la lista de clientes tiene 4 elementos
         self.assertEqual(nuevo_cliente.nombre, 'Héctor')   #se comprueba que el nombre del cliente es el que se le ha asignado
-        self.assertEqual(nuevo_cliente.apellido, 'Lopez') #se comprueba que los apellidos del cliente son los que se le han asignado
+        self.assertEqual(nuevo_cliente.apellido, 'Costa') #se comprueba que los apellidos del cliente son los que se le han asignado
 
     def test_modificar_cliente(self):
-        cliente_a_modificar=copy.copy(db.Clientes.buscar("15J")) #se crea una copia del cliente a modificar
-        cliente_modificado=db.Clientes.modificar("15J", "Hector", "Lopez") #se modifica el cliente
-        self.assertEqual(cliente_a_modificar.nombre, "Juan") #se comprueba que el nombre del cliente a modificar es Juan
-        self.assertEqual(cliente_modificado.nombre, "Hector") #se comprueba que el nombre del cliente modificado es Hector
+        cliente_a_modificar=copy.copy(db.Clientes.buscar("28Z")) #se crea una copia del cliente a modificar
+        cliente_modificado=db.Clientes.modificar("28Z", "Mariana", "Garcia") #se modifica el cliente
+        self.assertEqual(cliente_a_modificar.nombre, "Ana") #se comprueba que el nombre del cliente a modificar es Juan
+        self.assertEqual(cliente_modificado.nombre, "Mariana") #se comprueba que el nombre del cliente modificado es Hector
     
-    def test_eliminar_cliente(self):
-        cliente_borrado= db.Clientes.borrar("15J") #se elimina un cliente
-        cliente_rebuscado=db.Clientes.buscar("15J") #se busca el cliente eliminado
-        self.assertNotEqual(cliente_borrado, cliente_rebuscado) #se comprueba que el cliente borrado no es igual al cliente rebuscado
+    def test_borrar_cliente(self):
+        cliente_borrado= db.Clientes.borrar("48H") #se elimina un cliente
+        cliente_rebuscado=db.Clientes.buscar("48H") #se busca el cliente eliminado
+        self.assertEqual(cliente_borrado.dni, "48H") #se comprueba que el dni del cliente borrado es el que se le ha asignado
+        self.assertIsNone(cliente_rebuscado) #se comprueba que el cliente borrado no existe
 
     def test_dni_valido(self):
-        self.assertTrue(helpers.dni_valido('15J', db.Clientes.lista))
-        self.assertFalse(helpers.dni_valido('23223S', db.Clientes.lista))
+        self.assertTrue(helpers.dni_valido('00A', db.Clientes.lista))  #se comprueba si un dni es válido
+        self.assertFalse(helpers.dni_valido('232323S', db.Clientes.lista))
         self.assertFalse(helpers.dni_valido('F35', db.Clientes.lista))
-        self.assertFalse(helpers.dni_valido('48H', db.Clientes.lista))
-    
-    def test_escritura_csv(self):
-        db.Clientes.borrar('48H')
-        db.Clientes.borrar('15J')
-        db.Clientes.modificar('28Z', 'Mariana', 'García')
+        self.assertFalse(helpers.dni_valido('48H', db.Clientes.lista))  
+        
+    def test_escritura_csv(self):   #se comprueba que se puede escribir en el fichero csv
+        db.Clientes.borrar('48H')  #se borra un cliente
+        db.Clientes.borrar('15J')   #se borra otro cliente
+        db.Clientes.modificar('28Z', 'Mariana', 'García')   #se modifica un cliente
 
-        dni, nombre, apellido = None, None, None
-        with open(config.DATABASE_PATH, newline='\n') as fichero:
-            reader = csv.reader(fichero, delimiter=';')
-            dni, nombre, apellido = next(reader)
+        dni, nombre, apellido = None, None, None    #se inicializan las variables
+        with open(config.DATABASE_PATH, newline='\n') as fichero:   #se abre el fichero csv
+            reader = csv.reader(fichero, delimiter=';')   #se crea un lector de csv separado por ;
+            dni, nombre, apellido = next(reader)  #se leen los datos del primer cliente
 
-        self.assertEqual(dni, '28Z')
-        self.assertEqual(nombre, 'Mariana')
-        self.assertEqual(apellido, 'García')
+        self.assertEqual(dni, '28Z')  #se comprueba que el dni del cliente son los que se esperan
+        self.assertEqual(nombre, 'Mariana')    #se comprueba que el nombre del cliente son los que se esperan
+        self.assertEqual(apellido, 'García')   #se comprueba que los apellidos del cliente son los que se esperan
 
     
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main()    #se ejecuta el test
     
--- ui.py
+++ ui.py
@@ -93,10 +93,117 @@ class CreateClientWindow(Toplevel, CenterWidgetMixin):
             Label(frame, text="Apellidos").grid(row=0, column=2)
 
             dni=Entry(frame)
-            dni.grid
+            dni.grid(row=1, column=0)
+            nombre=Entry(frame)
+            nombre.grid(row=1, column=1)
+            nombre.bind("<KeyRelease>", lambda event: self.validate(event,0))
+            apellido= Entry(frame)
+            apellido.grid(row=1, column=2)
+            apellido.bind("<KeyRelease>", lambda event: self.validate(event,1))
 
-            
+            cliente=self.master.treeview.focus()
+            campos=self.master.treeview.item(cliente, 'values')
+            dni.insert(0, campos[0])
+            dni.configure(state=DISABLED)
+            nombre.insert(0, campos[1])
+            apellido.insert(0, campos[2])
 
-if __name__ == '__main__':
-    app=MainWindow()
+            frame=Frame(self)
+            frame.pack(pady=10)
+
+            actualizar=Button(frame, text="Actualizar", command=self.edit_client)
+            actualizar.grid(row=0, column=0)
+            Button(frame, text="Cancelar", command=self.close).grid(row=0, column=1)
+
+            self.validaciones=[1,1]
+            self.actualizar=actualizar
+            self.dni=dni
+            self.nombre=nombre
+            self.apellido=apellido
+
+        def edit_client(self):
+            cliente = self.master.treeview.focus()
+            self.master.treeview.item(cliente, values=(
+            self.dni.get(), self.nombre.get(), self.apellido.get()))
+            db.Clientes.modificar(self.dni.get(), self.nombre.get(), self.apellido.get())
+            self.close()
+
+        def close(self):
+            self.destroy()
+            self.update()
+
+        def validate(self, event, index):
+            valor = event.widget.get()
+            valido = (valor.isalpha() and len(valor) >= 2 and len(valor) <= 30)
+            event.widget.configure({"bg": "Green" if valido else "Red"})
+            # Cambiar el estado del botón en base a las validaciones
+            self.validaciones[index] = valido
+            self.actualizar.config(state=NORMAL if self.validaciones == [1, 1] else DISABLED)
+
+
+class MainWindow(Tk, CenterWidgetMixin):
+    def __init__(self):
+        super().__init__()
+        self.title("Gestor de clientes")
+        self.build()
+        self.center()
+
+    def build(self):
+        frame = Frame(self)
+        frame.pack()
+
+        treeview = ttk.Treeview(frame)
+        treeview['columns'] = ('DNI', 'Nombre', 'Apellido')
+
+        treeview.column("#0", width=0, stretch=NO)
+        treeview.column("DNI", anchor=CENTER)
+        treeview.column("Nombre", anchor=CENTER)
+        treeview.column("Apellido", anchor=CENTER)
+
+        treeview.heading("DNI", text="DNI", anchor=CENTER)
+        treeview.heading("Nombre", text="Nombre", anchor=CENTER)
+        treeview.heading("Apellido", text="Apellido", anchor=CENTER)
+
+        scrollbar = Scrollbar(frame)
+        scrollbar.pack(side=RIGHT, fill=Y)
+        treeview['yscrollcommand'] = scrollbar.set
+
+        for cliente in db.Clientes.lista:
+            treeview.insert(
+                parent='', index='end', iid=cliente.dni,
+                values=(cliente.dni, cliente.nombre, cliente.apellido))
+
+        treeview.pack()
+
+        frame = Frame(self)
+        frame.pack(pady=20)
+
+        Button(frame, text="Crear", command=self.create).grid(row=0, column=0)
+        Button(frame, text="Modificar", command=self.edit).grid(row=0, column=1)
+        Button(frame, text="Borrar", command=self.delete).grid(row=0, column=2)
+
+        self.treeview = treeview
+
+    def delete(self):
+        cliente = self.treeview.focus()
+        if cliente:
+            campos = self.treeview.item(cliente, "values")
+            confirmar = askokcancel(
+                title="Confirmar borrado",
+                message=f"¿Borrar {campos[1]} {campos[2]}?",
+                icon=WARNING)
+            if confirmar:
+                self.treeview.delete(cliente)
+                db.Clientes.borrar(campos[0])
+
+    def create(self):
+        CreateClientWindow(self)
+
+    def edit(self):
+        if self.treeview.focus():
+            EditClientWindow(self)
+
+
+if __name__ == "__main__":
+    app = MainWindow()
     app.mainloop()
